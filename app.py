2c5ce6eaef88b51946cf1e8ebaefe2218295f59f
naijun0403 <naijun0403@gmail.com>
2024-07-16 14:48:35 +0900

    feat: redirects

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
+from typing import Any
+
+import sanic
 from sanic import Sanic
 
 from api import api
+
+REDIRECTS = {
+    '/': '/docs/swagger'
+}
 
 app = Sanic("messengerbot-api")
 app.ext.openapi.describe(
@@ -12,3 +19,11 @@ app.ext.openapi.describe(
 app.config.OAS_UI_DEFAULT = 'swagger'
 
 app.blueprint(api)
+
+
+def get_static_function(value) -> Any:
+    return lambda *_, **__: value
+
+
+for src, dest in REDIRECTS.items():
+    app.route(src)(get_static_function(sanic.response.redirect(dest)))
